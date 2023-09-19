9c484068caf2f69a92388326b8047316167a5057
max fronek <35245841+maxfronek@users.noreply.github.com>
2023-09-19 01:38:23 -0700

    I made a menu! Also slight styling tweaks and homepage 'big link' updates

--- wedding_website/assets/js/page.js
+++ wedding_website/assets/js/page.js
@@ -1,20 +1,51 @@
 // page scripts
 
-/* remove JS hover for CSS solution
+//list as many as you'd like
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-let bigLinks = document.querySelectorAll('.big-links a');
+  // header hamburger
 
-bigLinks.forEach((link) => {
-    let correspondingGallery = link.nextElementSibling;
-    link.addEventListener('mouseover', () => {
-        correspondingGallery.classList.add('show');
-    });
+  let menuTrigger = document.getElementById('menuTrigger'),
+      menuOpen = false,
+      logo = document.getElementById('logo'),
+      menu = document.getElementById('main-menu');
 
-    link.addEventListener('mouseout', () => {
-        correspondingGallery.classList.remove('show');
-     });
+      if (menuOpen == false) {
+        menuTrigger.addEventListener("mouseover", () => {
+            menuTrigger.classList.add('hovered');
+        });
+
+        menuTrigger.addEventListener("mouseout", () => {
+            menuTrigger.classList.remove('hovered');
+        });
+    } // end if menu is not open
+
+        menuTrigger.addEventListener("click", () => {
+            if (menuOpen === false) {
+                console.log('Open trigger clicked');
+                menuTrigger.classList.add('opened');
+                menu.classList.add('opened');
+                logo.classList.add('reversed');
+                menuOpen = true;
+                console.log(menuOpen);
+            } else if (menuOpen === true) {
+                console.log('Close trigger clicked');
+                menuTrigger.classList.remove('opened');
+                menu.classList.remove('opened');
+                logo.classList.remove('reversed');
+                menuOpen = false;
+                console.log(menuOpen);
+            }
+        }); // end menuTrigger click event handler
 
 
+// header BG animations
+
+ScrollTrigger.create({
+    trigger: 'main',
+    // markers: true,
+    start: "200px top",
+    id: 'one',
+    toggleClass: { targets: ".site-header", className: "header-bg" }
   });
-
-  */
+ 
---
+++ wedding_website/pages/test.md
@@ -0,0 +1,12 @@
+---
+layout: testbed
+title:  "Testbed"
+date:   2023-09-17 15:03:50 -0700
+permalink: /test/
+cover: "/assets/images/vc/valleyclub-cover.jpg"
+---
+
+
+Test content for this page goes here so that you can see if th menu is applying the background effects properly.
+
+<h1>BIG ASS TITLE</h1>
